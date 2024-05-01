579f4d4a70c8d21cde202a9a821c2a6c9323778e
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-05-01 11:45:57 +0545

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -1,1 +1,18 @@
+using System;
 
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a first number");
+            double first = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter a second number");
+            double second = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(first + second);
+            Console.ReadLine();
+        }
+    }
+}
