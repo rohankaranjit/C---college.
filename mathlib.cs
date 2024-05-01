41e4d506249d7d974b937622e6584e92b8c8dbf4
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-05-01 11:54:08 +0545

    Update mathlib.cs

--- mathlib.cs
+++ mathlib.cs
@@ -1,1 +1,27 @@
+using System;
 
+namespace Mathlib
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.Write("Choose a color.");
+            string color = Console.ReadLine();
+
+            Console.Write("Choose a plural noun.");
+            string pluralnoun = Console.ReadLine();
+
+            Console.Write("Choose a celebrity you admire.");
+            string celebrity = Console.ReadLine();
+
+
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralnoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
