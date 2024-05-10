39e38d1b74a4c6c2eb4818844c82302375a407a1
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-05-10 23:43:00 +0545

    Update mathlib.cs

--- mathlib.cs
+++ mathlib.cs
@@ -10,7 +10,7 @@ namespace Mathlib
             Console.Write("Choose a plural noun.");
             string pluralnoun = Console.ReadLine();
 
-            Console.Write("Choose a celebrity you admire.");
+            Console.Write("Choose a celebrity you admire.")
             string celebrity = Console.ReadLine();
 
 
