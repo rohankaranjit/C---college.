e730a0c68cf15b22e86e56c6f35aec1b86dfb4c6
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-05-13 19:20:19 +0545

    Update Switch-whichday?.cs

--- Switch-whichday?.cs
+++ Switch-whichday?.cs
@@ -1,1 +1,45 @@
+using System;
 
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(GetDay(3));
+
+            Console.ReadLine();
+        }
+
+        static string GetDay(int DayNum)
+        {
+            string DayName;
+
+            switch (DayNum)
+            {
+                case 0:
+                    DayName = "Sunday";
+                    break;
+                case 1:
+                    DayName = "Monday";
+                    break;
+                case 2:
+                    DayName = "Tuesday";
+                    break;
+                case 3:
+                    DayName = "Wednesday";
+                    break;
+                default:
+                    DayName = "Invalid Day Number";
+                    break;
+            }
+            
+
+            return DayName;
+
+
+
+        }
+    }
+}
