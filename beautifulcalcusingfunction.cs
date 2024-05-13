90a45ea3dfb43cb754343b6491ba3bd574db968a
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-05-13 19:01:31 +0545

    Update and rename beautifulcalcusingfunction to beautifulcalcusingfunction.cs

---
+++ beautifulcalcusingfunction.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace beautifulcalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter the operator");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter the second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            Console.ReadLine();
+            }
+    }
+}
